bdbf6ceb2790952acb6efb2ff5ce978cd9a9ea51
Hari <haridhamo3@gmail.com>
2026-06-01 22:22:23 +0530

    ssrf update

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
-
+app.set('trust proxy', 1);
 /**
  * Example Express Rest API endpoints can be defined here.
  * Uncomment and define endpoints as necessary.
